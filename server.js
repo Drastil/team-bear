3091b90230f621c0b2bccdcaea762860ca50e5f3
Saransh Bhandari <saransh.bhandari77@gmail.com>
2019-01-29 22:58:59 -0600

    change output text in server file

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const port = 9000;
 express.static('static');
 
 app.get('/', function(req, res) {
-	res.send("hello from expressjs")
+	res.send("hello from express js")
 });
 
 app.listen(port, "0.0.0.0");
-console.log('listening to port' + port );
+console.log('listening to port at ' + port );
